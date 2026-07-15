4f6d02dd19254d774ed5f1cde27ed4d90e5b15a0
mahnooriftikhar-dev <mahnooriftikhar.dev@gmail.com>
2026-07-15 17:47:56 +0500

    Added JS

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-// const dicenumber = document.getElementById("dice-no").value;
-// const roll_dice = document.getElementById("roll-dice");
-// const result = document.getElementById("result");
-// const dice_images = document.getElementById("dice_images");
-
-// function roll() {
-//     //for storing multiple values in this array
-//     const values = [];
-//     //for storing multiple images in this array
-//     const images = [];
-//     //button click will start the function
-//     for (i = 0; i > dicenumber; i++){
-//     const value = Math.floor(Math.random *6)+1;
-//     value.push(values)
-//     images.push(`<img src="dice_images/${value}.png"></img>`)
-//     }
-
-//     result.textContent = `dice ${values.join(',')}`;
-//     dice_images.innerHTML=images.join('');
-// }
-
 const diceNumber = document.getElementById("dice-no");
 const result = document.getElementById("result");
 const diceImages = document.getElementById("dice_images");
